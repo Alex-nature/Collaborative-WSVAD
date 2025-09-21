e0159a1db06834cef08dc5024b71a23f1f0792dc
Alex-nature <cht312511@163.com>
2025-09-21 09:45:28 +0800

    commit config file

--- utils/config.py
+++ utils/config.py
@@ -1,11 +1,11 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-
+# 你好
 parser.add_argument("--dataset", type=str, default="ucf", choices=["ucf", "xd"])
 parser.add_argument("--batch_size", type=int, default=64, help="batch size")
-parser.add_argument("--learning_rate", type=float, default=1e-4, help="Local learning rate")
-parser.add_argument("--global_rounds", type=int, default=20, help="Global federated learning rounds")
+parser.add_argument("--learning_rate", type=float, default=1e-5, help="Local learning rate")
+parser.add_argument("--global_rounds", type=int, default=15, help="Global federated learning rounds")
 parser.add_argument("--local_epochs", type=int, default=10, help="local training epochs for each client")
 parser.add_argument("--algorithm", type=str, default="FedAvg", choices=["FedAvg", "FedProx", "Scaffold"])
 parser.add_argument("--clients_num", type=int, default=13, help="Number of client per round when random")
@@ -21,7 +21,7 @@ parser.add_argument('--visual_length', default=256, type=int, help="length of vi
 parser.add_argument('--visual_width', default=512, type=int)
 parser.add_argument('--visual_head', default=1, type=int)
 parser.add_argument('--visual_layers', default=2, type=int, help="number of visual layers, default 2 for ucf")
-
+# 没用到下面的参数
 parser.add_argument('--attn_window', default=8, type=int, help="size of attention window, default 8 for ucf")
 parser.add_argument('--prompt_prefix', default=10, type=int)
 parser.add_argument('--prompt_postfix', default=10, type=int)
